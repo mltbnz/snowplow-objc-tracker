729ba133c9b44736d8ea3d5f4fdc3c1ae1e87f46
Malte Buenz <malte.buenz@deutschebahn.com>
2019-12-16 10:40:25 +0100

    add SPM Package

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "SnowplowTracker",
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_10),
+        .watchOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "SnowplowTracker",
+            targets: ["SnowplowTracker"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ashleymills/Reachability.swift", from: "4.3.1"),
+        .package(url: "https://github.com/ccgus/fmdb", from: "2.6.2")
+    ],
+    targets: [
+        .target(
+            name: "SnowplowTracker",
+            path: "Snowplow",
+            dependencies: [])
+    ]
+)
